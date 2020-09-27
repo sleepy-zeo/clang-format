fea845a5caac38c1d224d6ed891a2569e590edf3
wuhongwei <hongwei.wu@grandsun.com>
2020-09-27 14:33:19 +0800

    xx

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
+#include <iostream>
+
+template <unsigned long N>
+struct binary {
+    static unsigned const value = binary<N / 10>::value * 2 + N % 10;
+};
+
+template <>
+struct binary<1> {
+    static unsigned const value = 1;
+};
+
+template <>
+struct binary<0> {
+    static unsigned const value = 0;
+};
+
+template <class X, class arg1,class arg2>
+X apply(X x,arg1 arg_1,arg2 arg_2){
+    return x(arg_1,arg_2);
+}
 
 int main() {
+
+    std::cout << binary<10010>::value << std::endl;
 
     return 0;
 }
